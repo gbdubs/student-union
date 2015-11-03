f807fbe0d0fa7397d07457929ef4962aace2711c
gradyberryward <gward@brandeis.edu>
2015-11-03 01:50:30 -0500

    Fixed Bylaws to be correct.

--- RUNME.sh
+++ RUNME.sh
@@ -4,16 +4,34 @@ printf "\n\n------------------------ UPDATING FROM GITHUB ----------------------
 
 git pull origin master
 
-printf "\n\n------------------------ GENERATING WEBSITE ------------------------\n\n"
+printf "\n\n------------------------ GENERATING WEBSITE PART 1 ------------------------\n\n"
 
 java GenerateWebsite
+sleep 5
+
+printf "\n\n------------------------ GENERATING WEBSITE PART 2 ------------------------\n\n"
+
+java GenerateWebsite
+sleep 5
 
 printf "\n\n------------------------ BEGINNING GIT OPS ------------------------\n\n"
 
 git add *
 NOW=$(date)
-read -p "Commit description: " desc  
 git commit -m "AUTOMATICALLY GENERATED COMMIT FROM $USER ON $NOW"
 git push origin gh-pages
 
-printf "\n\n------------------------ COMPLETED UPLOAD ------------------------\n\n"
+printf "\n\n------------------------ COMPLETED UPLOAD TO gbdubs/student-union ------------------------\n\n"
+
+
+printf "\n\n------------------------ Cloning into brandeisu.github.io ------------------------\n\n"
+
+cp -r * ../brandeisu.github.io/
+cd ../brandeisu.github.io
+git add *
+NOW=$(date) 
+git commit -m "AUTOMATICALLY GENERATED COMMIT FROM $USER ON $NOW"
+git push origin master
+
+printf "\n\n------------------------ COMPLETED UPLOAD TO brandeisu.github.io ------------------------\n\n"
+
